e2c9adf1ca492064426365d200dca9e66861f82f
j-a-w-n <webmasterconnolly@gmail.com>
2017-01-18 12:42:04 -0400

    Updated lab assesment

--- labs/sum_values.py
+++ labs/sum_values.py
@@ -3,12 +3,9 @@
 from sys import argv
 
 programmer = """
-Programmed by Kyle Connolly
+Programmed by Kyle Connolly\
 """
-
-print()
 print(programmer)
-for item in argv[1:]:
-    total = sum(int(item) for i in argv[1:])
+total = sum(int(i) for i in argv[1:])
 print("The sum is {}.".format(total))
 input("Press Enter to continue ... ")
