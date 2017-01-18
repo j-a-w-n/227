6487034c992b8bf3ff1c2f056f5ea8f87985f6d0
j-a-w-n <webmasterconnolly@gmail.com>
2017-01-18 12:19:07 -0400

    sum_values correction

--- labs/sum_values.py
+++ labs/sum_values.py
@@ -5,11 +5,10 @@ from sys import argv
 programmer = """
 Programmed by Kyle Connolly
 """
-# total = sum(int(i) for i in argv[1:])
 
 print()
 print(programmer)
 for item in argv[1:]:
-    int(item)
-    print("The sum is {} .".format(total))
+    total = sum(int(item) for i in argv[1:])
+print("The sum is {}.".format(total))
 input("Press Enter to continue ... ")
